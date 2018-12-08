5c164ace19ed447ef7de7739f42c9b983e0eeecb
Md. Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2018-12-08 13:16:59 +0600

    update project code

--- assignment/assignment_02/test_learn.py
+++ assignment/assignment_02/test_learn.py
@@ -1,63 +1,43 @@
-"""
-Make the imports of python packages needed
-"""
+
 import pandas as pd
 import numpy as np
 from pprint import pprint
 
 
-#Import the dataset and define the feature as well as the target datasets / columns#
-dataset = pd.read_csv('zoo.csv')#Import all columns omitting the fist which consists the names of the animals
-#We drop the animal names since this is not a good feature to split the data on
+
+ 
+dataset = pd.read_csv('car_evaluation.csv')
+
 columns = list(dataset.columns.values)
-print(columns)
-print(set(dataset[columns[0]].tolist()))
-print(dataset[columns[1]].dtypes)
+# print(columns)
+# print(set(dataset[columns[0]].tolist()))
+# print(dataset.dtypes)
 from functions import repair_dataset
 dataset = repair_dataset(dataset, .2, 4,.4)
 target_attribute_name = columns[-1]
-total_data = int(len(dataset[target_attribute_name].tolist()) * .8)
+total_data = int(len(dataset[target_attribute_name].tolist()) * .95)
 continuous_threshold = .2
 continuous_ignore = .4
 continuous_partition = 4
+# print(dataset)
 
 
 
-###########################################################################################################
-from functions import entropy
-########################################################################################################### 
-    
-###########################################################################################################
-from functions import InfoGain
-       
-###########################################################################################################
-###########################################################################################################
-from functions import ID3               
-###########################################################################################################
-###########################################################################################################
-    
-    
-from functions import predict
-        
-        
-"""
-Check the accuracy of our prediction.
-The train_test_split function takes the dataset as parameter which should be divided into
-a training and a testing set. The test function takes two parameters, which are the testing data as well as the tree model.
-"""
-###########################################################################################################
-###########################################################################################################
 
+
+
+# split data
 from functions import train_test_split
-
 training_data, testing_data = train_test_split(dataset, total_data)
 
 
-from functions import test
 
-"""
-Train the tree, Print the tree and predict the accuracy
-"""
+
+# create tree
+from functions import ID3               
 tree = ID3(training_data,training_data,training_data.columns[:-1], target_attribute_name)
-pprint(tree)
+# pprint(tree)
+
+# test
+from functions import test
 test(testing_data,tree,target_attribute_name)
